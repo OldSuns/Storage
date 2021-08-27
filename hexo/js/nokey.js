45a8ddae06e16327d969413ff5bc103333130107
OldSuns <1239502201@qq.com>
2021-08-27 14:20:27 +0800

    update

--- hexo/js/nokey.js
+++ hexo/js/nokey.js
@@ -1,55 +1,106 @@
-(function () {
-var re = /x/;
-var i = 0;
-console.log(re);
-  
-re.toString = function () {
-alert("请关闭控制台");
-return '第 ' + (++i) + ' 次打开控制台';
-};
-})();
+// 禁止打开控制台和右键
+function forbidden_control() {
+    $.extend({
+            message: function(a) {
+                var b = {
+                    title: "",
+                    message: "操作成功",
+                    time: "3000",
+                    type: "success",
+                    showClose: !0,
+                    autoClose: !0,
+                    onClose: function() {}
+                };
+                "string" == typeof a && (b.message = a), "object" == typeof a && (b = $.extend({}, b, a));
+                var c, d, e, f = b.showClose ? '<div class="c-message--close">×</div>' : "",
+                    g = "" !== b.title ? '<h2 class="c-message__title">' + b.title + "</h2>" : "",
+                    h = '<div class="c-message animated animated-lento slideInRight"><i class=" c-message--icon c-message--' + b.type + '"></i><div class="el-notification__group">' + g + '<div class="el-notification__content">' + b.message + "</div>" + f + "</div></div>",
+                    i = $("body"),
+                    j = $(h);
+                d = function() {
+                    j.addClass("slideOutRight"), j.one("webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend", function() {
+                        e()
+                    })
+                }, e = function() {
+                    j.remove(), b.onClose(b), clearTimeout(c)
+                }, $(".c-message").remove(), i.append(j), j.one("webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend", function() {
+                    j.removeClass("messageFadeInDown")
+                }), i.on("click", ".c-message--close", function(a) {
+                    d()
+                }), b.autoClose && (c = setTimeout(function() {
+                    d()
+                }, b.time))
+            }
+        }),
+        document.onkeydown = function(e) {
+            if (123 == e.keyCode || e.ctrlKey && e.shiftKey && (74 === e.keyCode || 73 === e.keyCode) || (e.ctrlKey && 85 === e.keyCode)) 
+				return $.message({
+                message: "F12躲起来啦",
+                title: "你真坏，不能打开控制台喔!",
+                type: "error",
+                autoHide: !1,
+                time: "5000"
+            }), event.keyCode = 0, event.returnValue = !1, !1
+        }, document.oncontextmenu = function() {
+            return $.message({
+                message: "右键菜单不见了？",
+                title: "不能右键喔！",
+                type: "error",
+                autoHide: !1,
+                time: "5000"
+            }), !1
+        },
+        function() {
+            function e() {
+                var e = new Date;
+                if (new Date - e > 10) {
+                    try {
+                        document.getElementsByTagName("html")[0].innerHTML = '<div style="width: 100%;height: 50px;font-size: 30px;text-align: center;font-weight: bold;">啊这个网站炸了，<a href="/" style="color:#4285f4;">点击返回</a>试试吧~</div>'
+                    } catch (e) {}
+                    return document.body.innerHTML = '<div style="width: 100%;height: 50px;font-size: 30px;text-align: center;font-weight: bold;">啊这个网站炸了，<a href="/" style="color:#4285f4;">点击返回</a>试试吧~</div>', !0
+                }
+                return !1
+            }
 
-setInterval(function() {
-  check()
-}, 4000);
-var check = function() {
-  function doCheck(a) {
-    if (("" + a/a)["length"] !== 1 || a % 20 === 0) {
-      (function() {}
-      ["constructor"]("debugger")())
-    } else {
-      (function() {}
-      ["constructor"]("debugger")())
-    }
-    doCheck(++a)
-  } 
-  try {
-    doCheck(0)
-  } catch (err) {}
-};
-check();
-
-document.onkeydown = function(){
-  if(window.event && window.event.keyCode == 123) {
-    alert("不能打开控制台喔!");
-    event.keyCode=0;
-    event.returnValue=false;
-  }
-  if(window.event && window.event.keyCode == 13) {
-    window.event.keyCode = 505;
-  }
-}
-document.oncontextmenu = function (event){
-if(window.event){
-event = window.event;
-}try{
-var the = event.srcElement;
-if (!((the.tagName == "INPUT" && the.type.toLowerCase() == "text") || the.tagName == "TEXTAREA")){
-return false;
-}
-return true;
-}catch (e){
-return false;
-}
+            function t() {
+                for (; e();) e()
+            }
+            e() ? t() : window.onblur = function() {
+                setTimeout(function() {
+                    t()
+                }, 500)
+            }
+        }(),
+        function() {
+            var e = /x/;
+            e.toString = function() {
+                try {
+                    document.getElementsByTagName("html")[0].innerHTML = '<div style="width: 100%;height: 50px;font-size: 30px;text-align: center;font-weight: bold;">啊这个网站炸了，<a href="/" style="color:#4285f4;">点击返回</a>试试吧~</div>'
+                } catch (e) {}
+                return document.body.innerHTML = '<div style="width: 100%;height: 50px;font-size: 30px;text-align: center;font-weight: bold;">啊这个网站炸了，<a href="/" style="color:#4285f4;">点击返回</a>试试吧~</div>', "禁止打开控制台！"
+            }
+        }(),
+        function() {
+            var e = document.createElement("div");
+            Object.defineProperty(e, "id", {
+                get: function() {
+                    try {
+                        document.getElementsByTagName("html")[0].innerHTML = '<div style="width: 100%;height: 50px;font-size: 30px;text-align: center;font-weight: bold;">啊这个网站炸了，<a href="/" style="color:#4285f4;">点击返回</a>试试吧~</div>'
+                    } catch (e) {}
+                    document.body.innerHTML = '<div style="width: 100%;height: 50px;font-size: 30px;text-align: center;font-weight: bold;">啊这个网站炸了，<a href="/" style="color:#4285f4;">点击返回</a>试试吧~</div>'
+                }
+            }), console.log(e)
+        }()
 }
 
+//如果用户在工具栏调起开发者工具，那么判断浏览器的可视高度和可视宽度是否有改变，如有改变则关闭本页面 
+var h = window.innerHeight,w=window.innerWidth; 
+window.onresize = function () { 
+    if (h!= window.innerHeight||w!=window.innerWidth){ 
+        window.close(); 
+        window.location = "about:blank"; 
+    } 
+}
+
+btf.isJqueryLoad(forbidden_control)
+//forbidden_control();
